552248160f2ac2a71e14e009852218786103b114
Alexey Radul <axch@mit.edu>
2017-02-06 23:49:13 -0500

    Draft an arithmetic operation.

--- src/Eval.hs
+++ src/Eval.hs
@@ -16,8 +16,12 @@ lookup :: Elem ctx t -> HL.HVect (TMap f ctx) -> f t
 lookup EZ = HL.head
 lookup (ES ind) = Eval.lookup ind . HL.tail
 
+arith :: (Num a) => Val (Exp.Real a) -> ArithOp (Exp.Real a) b -> Val (Exp.Real a) -> Val b
+arith (RealVal x1) Plus (RealVal x2) = RealVal $ x1 + x2
+
 eval :: Exp ctx t -> HL.HVect (TMap Val ctx) -> Val t
 eval (Var ind) env = Eval.lookup ind env
+eval (Arith e1 op e2) env = arith (eval e1 env) op (eval e2 env)
 eval (RealE (Exp.Real v)) _ = RealVal v
 eval (BoolE v) _ = BoolVal v
 
